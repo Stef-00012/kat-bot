9f96bf9d324714d177a9b29e5232eae8efcec14b
space <66379820+Linker-123@users.noreply.github.com>
2024-07-11 12:16:59 +0300

    fix

--- src/commands/fun/ban.ts
+++ src/commands/fun/ban.ts
@@ -1,36 +1,60 @@
-import { Client, ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder, PermissionFlagsBits } from "discord.js";
+import {
+  Client,
+  ChatInputCommandInteraction,
+  EmbedBuilder,
+  SlashCommandBuilder,
+  PermissionFlagsBits,
+  GuildMember,
+  User,
+} from "discord.js";
 
 export const command = new SlashCommandBuilder()
   .setName("fun")
   .setDescription("Funny haha commands")
-  .addSubcommand(subcommand =>
+  .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
+  .addSubcommand((subcommand) =>
     subcommand
-      .setName('ban')
-      .setDescription('Bans a user (not really tho)')
-      .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
-      .addUserOption(option => option.setName('user').setDescription('The user to (fake)ban'))
-      .addStringOption(option =>
-        option.setName('reason')
-          .setDescription('The reason for the (fake)ban')
+      .setName("ban")
+      .setDescription("Bans a user (not really tho)")
+      .addUserOption((option) =>
+        option.setName("user").setDescription("The user to (fake)ban")
+      )
+      .addStringOption((option) =>
+        option
+          .setName("reason")
+          .setDescription("The reason for the (fake)ban")
           .setMaxLength(2000)
       )
   );
 
-export async function execute(inter: ChatInputCommandInteraction, client: Client) {
-  const user = inter.options.getUser('user');
-  const reason = inter.options.getString('reason') ?? 'No reason provided';
+export async function execute(
+  inter: ChatInputCommandInteraction,
+  client: Client
+) {
+  if (!inter.member) return;
+
+  const user = inter.options.getUser("user");
+  const reason = inter.options.getString("reason") ?? "No reason provided";
 
   const fakeBanEmbed = new EmbedBuilder()
-    .setColor(0xFF0000)
-    .setTitle('The ban hammer has spoken')
-    .setAuthor({ name: inter.member.user.username, iconURL: inter.member.user.displayAvatarURL({ format: "png" }) })
-    .setDescription('The ban hammer has spoken and has taken action')
+    .setColor(0xff0000)
+    .setTitle("The ban hammer has spoken")
+    .setAuthor({
+      name: inter.member.user.username,
+      iconURL: (inter.member as GuildMember).displayAvatarURL({
+        extension: "png",
+      }),
+    })
+    .setDescription("The ban hammer has spoken and has taken action")
     .addFields(
-      { name: 'Member', value: user?.username ?? 'Unknown User' },
-      { name: 'Reason', value: reason },
+      { name: "Member", value: user?.username ?? "Unknown User" },
+      { name: "Reason", value: reason }
     )
     .setTimestamp()
-    .setFooter({ text: 'KatBot', iconURL: client.user?.displayAvatarURL({ format: "png" }) ?? '' });
+    .setFooter({
+      text: "KatBot",
+      iconURL: (client.user! as User).displayAvatarURL({ extension: "png" }) ?? "",
+    });
 
   await inter.reply({ embeds: [fakeBanEmbed] });
 }
